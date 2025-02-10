b11898f4e51f3eb6d12091a1629342e03d6f7288
RomanHlodann <roman.hlodann@gmail.com>
2025-02-10 14:54:09 +0200

    add middlware to check if object of provided schema is related to current user

---
+++ src/middlewares/is-object-related-to-user.js
@@ -0,0 +1,18 @@
+function isObjectRelatedToUser(schema) {
+  return async (req, res, next) => {
+    try {
+        const object = await schema.findById(req.params.id);
+        if (!object || !object.user || object.user.toString() !== req.user._id) {
+            return res.status(404).send({'message': 'Machine not found'});
+        }
+
+        req.neededObject = object;
+
+        next();
+    } catch (err) {
+        next(err);
+    }
+  };
+}
+
+module.exports = isObjectRelatedToUser;
