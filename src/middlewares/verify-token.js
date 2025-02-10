faaa30038c80683955eabeb1f6c36d54cf8eded5
RomanHlodann <roman.hlodann@gmail.com>
2025-02-11 00:58:03 +0200

    remove Bearer when using token from outside

--- src/middlewares/verify-token.js
+++ src/middlewares/verify-token.js
@@ -4,10 +4,12 @@ const jwt = require('jsonwebtoken');
 
 
 module.exports = (req, res, next) => {
-    const token = req.headers['authorization'];
+    let token = req.headers['authorization'];
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
+
+    token = token.replace('Bearer ', '');
   
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
       if (err) {
